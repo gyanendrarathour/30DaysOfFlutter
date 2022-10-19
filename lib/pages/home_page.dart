0f02958ef24b9c2e2b192bc18355182450a1cc3f
gyanendrarathour <krishurathour1992@gmail.com>
2022-10-19 20:11:01 +0530

    Day 3 Completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = 'Gyanendra Singh';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calalog App'),
+      ),
+      body: Container(
+        child: Center(child: Text('Welcome to $days days of Flutter by $name')),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
