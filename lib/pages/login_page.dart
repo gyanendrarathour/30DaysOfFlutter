dc94b336721d4b5e71cabc0e0ff4aedde9d3586a
gyanendrarathour <krishurathour1992@gmail.com>
2022-10-20 00:21:33 +0530

    Day 5 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,46 +7,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          Image.asset(
-            'images/login_image.png',
-            fit: BoxFit.cover,
-            height: 150,
-            width: 200,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            'Welcome',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              children: [
-                const TextField(
-                  decoration: InputDecoration(
-                      hintText: "Enter User Name", label: Text('User Name')),
-                ),
-                const TextField(
-                  obscureText: true,
-                  obscuringCharacter: '*',
-                  decoration: InputDecoration(
-                      hintText: "Enter Password", label: Text('Password')),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(onPressed: () {}, child: const Text('Login'))
-              ],
+      child: SafeArea(
+        child: Column(
+          children: [
+            Image.asset(
+              'images/login_image.png',
+              fit: BoxFit.cover,
+              height: 150,
+              width: 200,
             ),
-          )
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Welcome',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                        hintText: "Enter User Name", label: Text('User Name')),
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", label: Text('Password')),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    style:
+                        TextButton.styleFrom(minimumSize: const Size(150, 40)),
+                    child: const Text('Login'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
