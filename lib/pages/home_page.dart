2c32267054fef4f16d19cc500245686edee0845f
gyanendrarathour <krishurathour1992@gmail.com>
2022-10-25 18:24:19 +0530

    Day 12 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/itemWidget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String name = 'Gyanendra Singh';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    final catalogJson =
+        await rootBundle.loadString('assets/files/catalog.json');
+    final decodedData = jsonDecode(catalogJson);
+    var productData = decodedData['products'];
+    print(productData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    String name = 'Gyanendra Singh';
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calalog App'),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -35,7 +35,7 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             children: [
               Image.asset(
-                'images/login_image.png',
+                'assets/images/login_image.png',
                 fit: BoxFit.cover,
                 height: 150,
                 width: 200,
